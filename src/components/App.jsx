f4edfb5dfe091027b7228af2aed27ce5b40ddc4a
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-21 21:20:39 +0200

    first steps to start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import React from 'react';
+import specifications from '../data/spesifications.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ul>
+        {specifications.map(spec => {
+          return (
+            <li key={spec.id}>
+              <p>
+                {spec.nearby.split('\n').map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    <br />
+                  </React.Fragment>
+                ))}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
