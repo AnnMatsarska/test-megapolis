ffe954c2e4ef158aad793d173ceaa9b947521a05
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-21 22:09:11 +0200

    add fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import specifications from '../data/spesifications.json';
+// import React from 'react';
+// import specifications from '../data/spesifications.json';
+import { Header } from '../components/Header/Header';
 
 export const App = () => {
   return (
-    <div>
-      <ul>
-        {specifications.map(spec => {
-          return (
-            <li key={spec.id}>
-              <p>
-                {spec.nearby.split('\n').map((line, index) => (
-                  <React.Fragment key={index}>
-                    {line}
-                    <br />
-                  </React.Fragment>
-                ))}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
