0077fb93e03b714d34f068704e4f2a5d83950afd
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-22 13:40:08 +0200

    add review section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { Header } from '../components/Header/Header';
 import { Hero } from '../components/Hero/Hero';
 import { Options } from '../components/Options/Options';
 import { Description } from '../components/Description/Description';
+import { Reviews } from '../components/Reviews/Reviews';
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
       <Hero />
       <Options />
       <Description />
+      <Reviews />
     </>
   );
 };
---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import css from './Reviews.module.css';
+import reviews from '../../data/reviews.json';
+import { ReactComponent as Location } from '../../images/icons/locationPointer.svg';
+
+export const Reviews = () => {
+  return (
+    <div className="container">
+      <section className={css.reviewsSection}>
+        <h2 className={css.reviewTitle}>Відгуки</h2>
+        <ul>
+          {reviews.map(review => {
+            return (
+              <>
+                <li key={review.id} className={css.reviewItem}>
+                  <div className={css.avatar}></div>
+                  <div className={css.infoUser}>
+                    <p>{review.name}</p>
+                    <div style={{ display: 'flex' }}>
+                      <Location />
+                      <p>{review.location}</p>
+                    </div>
+                  </div>
+                  <p className={css.reviewText}>{review.review}</p>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
+  );
+};
