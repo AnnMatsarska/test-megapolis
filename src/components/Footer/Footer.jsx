c5a8d161337a324cdca88005dd3ef339cba08dc6
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-22 16:26:37 +0200

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,46 +7,48 @@ export const Footer = () => {
   return (
     <section className={css.footerSection}>
       <div className="container">
-        <ul className={css.menuList}>
-          <li>
-            <a href="##">Переваги</a>
-          </li>
-          <li>
-            <a href="##">Адреси хостелів</a>
-          </li>
-          <li>
-            <a href="##">Відгуки</a>
-          </li>
-          <li>
-            <a href="##">UA </a> <DownArrow />
-          </li>
-        </ul>
-        <div className={css.telWrapper}>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px',
-            }}
-          >
-            <Phone style={{ width: '35px', height: '35px' }} />
+        <div className={css.wrapper}>
+          <ul className={css.menuList}>
+            <li>
+              <a href="##">Переваги</a>
+            </li>
+            <li>
+              <a href="##">Адреси хостелів</a>
+            </li>
+            <li>
+              <a href="##">Відгуки</a>
+            </li>
+            <li>
+              <a href="##">UA </a> <DownArrow />
+            </li>
+          </ul>
+          <div className={css.telWrapper}>
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '10px',
+              }}
+            >
+              <Phone style={{ width: '35px', height: '35px' }} />
+              <ul className={css.menuTelList}>
+                <li>
+                  <a href="tel:+380638903030">+38 (063) 890 30 30</a>
+                </li>
+                <li>
+                  <a href="tel:+380968903030">+38 (096) 890 30 30</a>
+                </li>
+              </ul>
+            </div>
             <ul className={css.menuTelList}>
               <li>
-                <a href="tel:+380638903030">+38 (063) 890 30 30</a>
+                <a href="tel:+38(066)8903030">+38 (066) 890 30 30</a>
               </li>
-              <li>
-                <a href="tel:+380968903030">+38 (096) 890 30 30</a>
+              <li className={css.recallLink}>
+                <a href="tel:+380968903030">Передзвонити</a>
               </li>
             </ul>
           </div>
-          <ul className={css.menuTelList}>
-            <li>
-              <a href="tel:+38(066)8903030">+38 (066) 890 30 30</a>
-            </li>
-            <li className={css.recallLink}>
-              <a href="tel:+380968903030">Передзвонити</a>
-            </li>
-          </ul>
         </div>
       </div>
     </section>
