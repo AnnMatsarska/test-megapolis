5e6a084e2f231c3cd53cb6b9de571bbc7e9f2c1c
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-22 16:56:02 +0200

    add new section

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,6 +4,8 @@ import { ReactComponent as User } from '../../images/icons/user.svg';
 import { ReactComponent as TwoUsers } from '../../images/icons/twoUsers.svg';
 import { ReactComponent as ThreeUsers } from '../../images/icons/threeUsers.svg';
 import { ReactComponent as FourUsers } from '../../images/icons/fourUsers.svg';
+import { ReactComponent as Selected } from '../../images/icons/selected.svg';
+import { ReactComponent as NotSelected } from '../../images/icons/notSelected.svg';
 
 export const Options = () => {
   return (
@@ -85,6 +87,57 @@ export const Options = () => {
           </div>
         </li>
       </ul>
+      <div className={css.moreInfoBox}>
+        <h2 className={css.optionsTitle}>Варіанти розміщення</h2>
+        <div className={css.typeWrapper}>
+          <div className={`container ${css.box}`}>
+            <p className={css.typeText}>Тип номеру</p>
+            <div className={css.priceWrapper}>
+              <p>Вартість за 1 місце</p>
+              <ul className={css.priceList}>
+                <li>Доба</li>
+                <li>Тиждень</li>
+                <li>4 Тижні</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={css.usersBar}>
+          <div className={css.leftBar}>
+            <div className={css.wrap}>
+              <Selected />
+              <User className={css.iconUser} />
+            </div>
+            <div className={css.wrap}>
+              <NotSelected />
+              <TwoUsers className={css.iconTwoUsers} />
+            </div>
+            <div className={css.wrap}>
+              <NotSelected />
+              <ThreeUsers className={css.iconThreeUsers} />
+            </div>
+            <div className={css.wrap}>
+              <NotSelected />
+              <FourUsers className={css.iconFourUsers} />
+            </div>
+          </div>
+          <div className={css.rightBar}>
+            <div className={css.rightbox}>
+              <div className={css.orderPriceBox}>
+                <p>430</p>
+                <p>1500</p>
+                <p>4500</p>
+              </div>
+              <button type="button" className={css.orderBtn}>
+                Забронювати
+              </button>
+            </div>
+            <div className={css.imgWpar}>
+              <p className={css.imgText}>Зображення номеру</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
