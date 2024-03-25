6ed8227e3d8b741d853b67902a8eacd4bd7f7a9a
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-03-25 18:04:46 +0200

    refactor description item component

--- src/components/Description/ListItem.jsx
+++ src/components/Description/ListItem.jsx
@@ -4,31 +4,31 @@ import hostel from '../../images/hostel.jpg';
 import { ReactComponent as Plus } from '../../images/icons/plus.svg';
 import { ReactComponent as Minus } from '../../images/icons/minus.svg';
 
+const ToggleItem = ({ isOpen, toggle, children }) => (
+  <>
+    {children}
+    {isOpen ? (
+      <Minus onClick={toggle} className={css.plusMinusIcon} />
+    ) : (
+      <Plus onClick={toggle} className={css.plusMinusIcon} />
+    )}
+  </>
+);
+
 export const ListItem = ({ spec }) => {
-  const [isOpenAddress, setIsOpenAddress] = useState(false);
-  const [isOpenPrice, setIsOpenPrice] = useState(false);
-  const [isOpenDescription, setIsOpenDescription] = useState(false);
-  const [isOpenNearby, setIsOpenNearby] = useState(false);
-  const [isOpenTransport, setIsOpenTransport] = useState(false);
+  const [isOpen, setIsOpen] = useState({
+    address: false,
+    price: false,
+    description: false,
+    nearby: false,
+    transport: false,
+  });
 
-  const toggleAddress = () => {
-    setIsOpenAddress(!isOpenAddress);
-  };
-
-  const togglePrice = () => {
-    setIsOpenPrice(!isOpenPrice);
-  };
-
-  const toggleDescription = () => {
-    setIsOpenDescription(!isOpenDescription);
-  };
-
-  const toggleNearby = () => {
-    setIsOpenNearby(!isOpenNearby);
-  };
-
-  const toggleTransport = () => {
-    setIsOpenTransport(!isOpenTransport);
+  const toggleOpen = key => {
+    setIsOpen(prevState => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
   };
 
   return (
@@ -39,138 +39,56 @@ export const ListItem = ({ spec }) => {
           <p className={css.new}>NEW</p>
           <p className={css.sale}>Акція</p>
         </div>
-        <div className={css.infoWrpper}>
-          <div className={css.box}>
-            <p
-              className={css.itemText}
-              style={{ color: isOpenAddress ? 'green' : 'inherit' }}
-            >
-              Адреса
-            </p>
-            {isOpenAddress ? (
-              <Minus onClick={toggleAddress} className={css.plusMinusIcon} />
-            ) : (
-              <Plus onClick={toggleAddress} className={css.plusMinusIcon} />
-            )}
-          </div>
-          {isOpenAddress && (
-            <p className={css.addressBox}>
-              {spec.address.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  <br />
-                </React.Fragment>
-              ))}
-            </p>
-          )}
-        </div>
-        <div className={css.infoWrpper}>
-          <div className={css.box}>
-            <p
-              className={css.itemText}
-              style={{ color: isOpenPrice ? 'green' : 'inherit' }}
-            >
-              Ціна
-            </p>
-            {isOpenPrice ? (
-              <Minus onClick={togglePrice} className={css.plusMinusIcon} />
-            ) : (
-              <Plus onClick={togglePrice} className={css.plusMinusIcon} />
-            )}
-          </div>
-          {isOpenPrice && (
-            <div className={css.priceList}>
-              {spec.price.map(pr => {
-                return (
-                  <div key={spec.id}>
-                    <p>доба: {pr.day}грн.</p>
-                    <p>
-                      тиждень:{' '}
-                      <span className={css.crossPrice}>{pr.week}грн</span>
-                      <span className={css.greenPrice}>від 620 грн.</span>
-                    </p>
-                    <p>
-                      4 тижня:{' '}
-                      <span className={css.crossPrice}>{pr.month}грн</span>
-                      <span className={css.greenPrice}>від 1900 грн.</span>
-                    </p>
-                  </div>
-                );
-              })}
+        {Object.entries(isOpen).map(([key, value]) => (
+          <div key={key} className={css.infoWrpper}>
+            <div className={css.box}>
+              <p
+                className={css.itemText}
+                style={{ color: value ? 'green' : 'inherit' }}
+                onClick={() => toggleOpen(key)}
+              >
+                {key === 'address' && 'Адреса'}
+                {key === 'price' && 'Ціна'}
+                {key === 'description' && 'Опис'}
+                {key === 'nearby' && 'Що поруч?'}
+                {key === 'transport' && 'Транспорт'}
+              </p>
+              <ToggleItem isOpen={value} toggle={() => toggleOpen(key)}>
+                {null}
+              </ToggleItem>
             </div>
-          )}
-        </div>
-        <div className={css.infoWrpper}>
-          <div className={css.box}>
-            <p
-              className={css.itemText}
-              style={{ color: isOpenDescription ? 'green' : 'inherit' }}
-            >
-              Опис
-            </p>
-            {isOpenDescription ? (
-              <Minus
-                onClick={toggleDescription}
-                className={css.plusMinusIcon}
-              />
-            ) : (
-              <Plus onClick={toggleDescription} className={css.plusMinusIcon} />
+            {value && (
+              <div className={css.descrptBox}>
+                {key !== 'price' ? (
+                  <p>
+                    {spec[key].split('\n').map((line, index) => (
+                      <React.Fragment key={index}>
+                        {line}
+                        <br />
+                      </React.Fragment>
+                    ))}
+                  </p>
+                ) : (
+                  spec[key].map((pr, index) => (
+                    <div key={index}>
+                      <p>доба: {pr.day}грн.</p>
+                      <p>
+                        тиждень:{' '}
+                        <span className={css.crossPrice}>{pr.week}грн</span>
+                        <span className={css.greenPrice}>від 620 грн.</span>
+                      </p>
+                      <p>
+                        4 тижня:{' '}
+                        <span className={css.crossPrice}>{pr.month}грн</span>
+                        <span className={css.greenPrice}>від 1900 грн.</span>
+                      </p>
+                    </div>
+                  ))
+                )}
+              </div>
             )}
           </div>
-          {isOpenDescription && (
-            <p className={css.descrptBox}>{spec.description}</p>
-          )}
-        </div>
-        <div className={css.infoWrpper}>
-          <div className={css.box}>
-            <p
-              className={css.itemText}
-              style={{ color: isOpenNearby ? 'green' : 'inherit' }}
-            >
-              Що поруч?
-            </p>
-            {isOpenNearby ? (
-              <Minus onClick={toggleNearby} className={css.plusMinusIcon} />
-            ) : (
-              <Plus onClick={toggleNearby} className={css.plusMinusIcon} />
-            )}
-          </div>
-          {isOpenNearby && (
-            <p className={css.descrptBox}>
-              {spec.nearby.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  <br />
-                </React.Fragment>
-              ))}
-            </p>
-          )}
-        </div>
-        <div className={css.infoWrpper}>
-          <div className={css.box}>
-            <p
-              className={css.itemText}
-              style={{ color: isOpenTransport ? 'green' : 'inherit' }}
-            >
-              Транспорт
-            </p>
-            {isOpenTransport ? (
-              <Minus onClick={toggleTransport} className={css.plusMinusIcon} />
-            ) : (
-              <Plus onClick={toggleTransport} className={css.plusMinusIcon} />
-            )}
-          </div>
-          {isOpenTransport && (
-            <p className={css.descrptBox}>
-              {spec.transport.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  <br />
-                </React.Fragment>
-              ))}
-            </p>
-          )}
-        </div>
+        ))}
       </div>
       <div className={css.endWrapper}>
         <p className={css.mapText}>Показати на мапі</p>
